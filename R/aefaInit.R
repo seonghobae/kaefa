8d6d219b2cee3babb4da50a5c9eb53d5e28a356a
seonghobae <seongho@kw.ac.kr>
2017-09-09 19:42:05 +0000

    align code with formatR::tidy_dir() and add packrat for InteliJ IDE environment

--- R/aefaInit.R
+++ R/aefaInit.R
@@ -15,28 +15,24 @@
 #'
 #'}
 aefaInit <- function(GCEvms = NULL, debug = F) {
-  options(future.debug = debug)
-
-  # setting up cluster
-  if (!is.null(GCEvms)) {
-    parallel::mclapply(future::plan(list(
-      conn <- future::tweak(future::cluster, workers = future::as.cluster(GCEvms)),
-      future::multiprocess
-    )), mc.cores = length(GCEvms))
-  } else if (NROW(future::plan("list")) == 1) {
-    if (length(grep('openblas', extSoftVersion()["BLAS"])) > 0) {
-      conn <- future::plan(future::multiprocess)
-    } else if (length(future::availableWorkers()) == 1) {
-      conn <- future::plan(future::sequential)
-    } else {
-      conn <- suppressMessages(try(future::plan(strategy = list(
-        future::tweak(future::cluster), future::multiprocess
-      )), silent = T)
-      )
+    options(future.debug = debug)
+    
+    # setting up cluster
+    if (!is.null(GCEvms)) {
+        parallel::mclapply(future::plan(list(conn <- future::tweak(future::cluster, workers = future::as.cluster(GCEvms)), 
+            future::multiprocess)), mc.cores = length(GCEvms))
+    } else if (NROW(future::plan("list")) == 1) {
+        if (length(grep("openblas", extSoftVersion()["BLAS"])) > 0) {
+            conn <- future::plan(future::multiprocess)
+        } else if (length(future::availableWorkers()) == 1) {
+            conn <- future::plan(future::sequential)
+        } else {
+            conn <- suppressMessages(try(future::plan(strategy = list(future::tweak(future::cluster), 
+                future::multiprocess)), silent = T))
+        }
+        
     }
-
-  }
-
-  return(conn)
-
+    
+    return(conn)
+    
 }
--- R/tools.R
+++ R/tools.R
@@ -23,99 +23,56 @@
 #'testModel1 <- fitMLIRT(mirt::Science, covdata = mirt::Science, random = list())
 #'
 #'}
-fitMLIRT <-
-  function(data = data,
-           model = model,
-           itemtype = NULL,
-           accelerate = accelerate,
-           GenRandomPars = GenRandomPars,
-           NCYCLES = NCYCLES,
-           BURNIN = BURNIN,
-           SEMCYCLES = SEMCYCLES,
-           symmetric = symmetric,
-           covdata = covdata,
-           fixed = fixed,
-           random = random) {
-    modMLIRT_itemLevel <- mirt::mixedmirt(
-      data = data,
-      model = model,
-      accelerate = accelerate,
-      itemtype = itemtype,
-      SE = T,
-      GenRandomPars = GenRandomPars,
-      covdata = covdata,
-      fixed = fixed,
-      random = random,
-      calcNull = T,
-      technical = list(
-        NCYCLES = NCYCLES,
-        BURNIN = BURNIN,
-        SEMCYCLES = SEMCYCLES,
-        symmetric = symmetric
-      )
-    )
-    modMLIRT_latentLevel <- mirt::mixedmirt(
-      data = data,
-      model = model,
-      accelerate = accelerate,
-      itemtype = itemtype,
-      SE = T,
-      GenRandomPars = GenRandomPars,
-      covdata = covdata,
-      lr.fixed = fixed,
-      lr.random = random,
-      calcNull = T,
-      technical = list(
-        NCYCLES = NCYCLES,
-        BURNIN = BURNIN,
-        SEMCYCLES = SEMCYCLES,
-        symmetric = symmetric
-      )
-    )
-
+fitMLIRT <- function(data = data, model = model, itemtype = NULL, accelerate = accelerate, GenRandomPars = GenRandomPars, 
+    NCYCLES = NCYCLES, BURNIN = BURNIN, SEMCYCLES = SEMCYCLES, symmetric = symmetric, covdata = covdata, 
+    fixed = fixed, random = random) {
+    modMLIRT_itemLevel <- mirt::mixedmirt(data = data, model = model, accelerate = accelerate, itemtype = itemtype, 
+        SE = T, GenRandomPars = GenRandomPars, covdata = covdata, fixed = fixed, random = random, calcNull = T, 
+        technical = list(NCYCLES = NCYCLES, BURNIN = BURNIN, SEMCYCLES = SEMCYCLES, symmetric = symmetric))
+    modMLIRT_latentLevel <- mirt::mixedmirt(data = data, model = model, accelerate = accelerate, itemtype = itemtype, 
+        SE = T, GenRandomPars = GenRandomPars, covdata = covdata, lr.fixed = fixed, lr.random = random, 
+        calcNull = T, technical = list(NCYCLES = NCYCLES, BURNIN = BURNIN, SEMCYCLES = SEMCYCLES, symmetric = symmetric))
+    
     # evaluate model
-    if (exists('modMLIRT_itemLevel')) {
-      if (class(modMLIRT_itemLevel) != 'list') {
-        if (!modMLIRT_itemLevel@OptimInfo$secondordertest) {
-          rm(modMLIRT_itemLevel)
+    if (exists("modMLIRT_itemLevel")) {
+        if (class(modMLIRT_itemLevel) != "list") {
+            if (!modMLIRT_itemLevel@OptimInfo$secondordertest) {
+                rm(modMLIRT_itemLevel)
+            }
+        } else {
+            rm(modMLIRT_itemLevel)
         }
-      } else {
-        rm(modMLIRT_itemLevel)
-      }
-
+        
     }
-
-
-    if (exists('modMLIRT_latentLevel')) {
-      if (class(modMLIRT_latentLevel) != 'list') {
-        if (!modMLIRT_latentLevel@OptimInfo$secondordertest) {
-          rm(modMLIRT_latentLevel)
+    
+    
+    if (exists("modMLIRT_latentLevel")) {
+        if (class(modMLIRT_latentLevel) != "list") {
+            if (!modMLIRT_latentLevel@OptimInfo$secondordertest) {
+                rm(modMLIRT_latentLevel)
+            }
+        } else {
+            rm(modMLIRT_latentLevel)
         }
-      } else {
-        rm(modMLIRT_latentLevel)
-      }
-
+        
     }
-
+    
     # decision
-    if (exists('modMLIRT_itemLevel') &&
-        exists('modMLIRT_latentLevel')) {
-      if (modMLIRT_itemLevel@Fit$DIC < modMLIRT_latentLevel@Fit$DIC) {
+    if (exists("modMLIRT_itemLevel") && exists("modMLIRT_latentLevel")) {
+        if (modMLIRT_itemLevel@Fit$DIC < modMLIRT_latentLevel@Fit$DIC) {
+            return(modMLIRT_itemLevel)
+        } else {
+            return(modMLIRT_latentLevel)
+        }
+    } else if (exists("modMLIRT_itemLevel") && !exists("modMLIRT_latentLevel")) {
         return(modMLIRT_itemLevel)
-      } else {
+    } else if (!exists("modMLIRT_itemLevel") && exists("modMLIRT_latentLevel")) {
         return(modMLIRT_latentLevel)
-      }
-    } else if (exists('modMLIRT_itemLevel') &&
-               !exists('modMLIRT_latentLevel')) {
-      return(modMLIRT_itemLevel)
-    } else if (!exists('modMLIRT_itemLevel') &&
-               exists('modMLIRT_latentLevel')) {
-      return(modMLIRT_latentLevel)
     } else {
-      # stop('no solution')
+        # stop('no solution')
     }
-
-  }
+    
+}
 
 
 #' assessment of fit indices of the calibrated model
@@ -132,122 +89,77 @@ fitMLIRT <-
 #' testModel1 <- estIRT(mirt::Science)
 #' testItemFit1 <- evaluateItemFit(testModel1)
 #' }
-evaluateItemFit <-
-  function(mirtModel,
-           GCEvms = NULL,
-           rotate = 'bifactorQ') {
-
-    if(!exists('.conn')){
-      .conn <- aefaInit(GCEvms = GCEvms, debug = getOption('future.debug' , FALSE))
+evaluateItemFit <- function(mirtModel, GCEvms = NULL, rotate = "bifactorQ") {
+    
+    if (!exists(".conn")) {
+        .conn <- aefaInit(GCEvms = GCEvms, debug = getOption("future.debug", FALSE))
     }
-
-    if (attr(class(mirtModel), 'package') == 'mirt') {
-      # item fit evaluation
-      modFit_Zh <- listenv()
-      modFit_Zh %<-% try(mirt::itemfit(
-        mirtModel,
-        rotate = rotate,
-        fit_stats = 'Zh',
-        QMC = T,
-        method = 'MAP',
-        impute = if (sum(is.na(mirtModel@Data$data)) > 0)
-          100
-        else
-          0
-      ),
-      silent = T)
-
-      modFit_SX2 <- listenv()
-      modFit_SX2 %<-% try(mirt::itemfit(
-        mirtModel,
-        rotate = rotate,
-        fit_stats = 'S_X2',
-        QMC = T,
-        method = 'MAP',
-        impute = if (sum(is.na(mirtModel@Data$data)) > 0)
-          100
-        else
-          0
-      ),
-      silent = T)
-
-      if (mirtModel@Model$nfact == 1) {
-        modFit_PVQ1 <- listenv()
-        modFit_PVQ1 %<-% try(mirt::itemfit(
-          mirtModel,
-          rotate = rotate,
-          fit_stats = 'PV_Q1',
-          QMC = T,
-          method = 'MAP'
-        ),
-        silent = T)
-
-      }
-
-      if (sum(mirtModel@Model$itemtype %in% 'Rasch') > 0 &&
-          mirtModel@Model$nfact == 1) {
-        modFit_infit <- listenv()
-        modFit_infit %<-% try(mirt::itemfit(
-          mirtModel,
-          rotate = rotate,
-          fit_stats = 'infit',
-          QMC = T,
-          method = 'MAP',
-          impute = if (sum(is.na(mirtModel@Data$data)) > 0)
-            100
-          else
-            0
-        ),
-        silent = T)
-      }
-
-      # check item fit indices are exists
-
-      if (exists('modFit_Zh')) {
-        if (!class(modFit_Zh)[1] == 'mirt_df') {
-          rm(modFit_Zh)
+    
+    if (attr(class(mirtModel), "package") == "mirt") {
+        # item fit evaluation
+        modFit_Zh <- listenv()
+        modFit_Zh %<-% try(mirt::itemfit(mirtModel, rotate = rotate, fit_stats = "Zh", QMC = T, method = "MAP", 
+            impute = if (sum(is.na(mirtModel@Data$data)) > 0) 
+                100 else 0), silent = T)
+        
+        modFit_SX2 <- listenv()
+        modFit_SX2 %<-% try(mirt::itemfit(mirtModel, rotate = rotate, fit_stats = "S_X2", QMC = T, method = "MAP", 
+            impute = if (sum(is.na(mirtModel@Data$data)) > 0) 
+                100 else 0), silent = T)
+        
+        if (mirtModel@Model$nfact == 1) {
+            modFit_PVQ1 <- listenv()
+            modFit_PVQ1 %<-% try(mirt::itemfit(mirtModel, rotate = rotate, fit_stats = "PV_Q1", QMC = T, 
+                method = "MAP"), silent = T)
+            
         }
-      }
-
-      if (exists('modFit_SX2')) {
-        if (!class(modFit_SX2)[1] == 'mirt_df') {
-          rm(modFit_SX2)
+        
+        if (sum(mirtModel@Model$itemtype %in% "Rasch") > 0 && mirtModel@Model$nfact == 1) {
+            modFit_infit <- listenv()
+            modFit_infit %<-% try(mirt::itemfit(mirtModel, rotate = rotate, fit_stats = "infit", QMC = T, 
+                method = "MAP", impute = if (sum(is.na(mirtModel@Data$data)) > 0) 
+                  100 else 0), silent = T)
         }
-      }
-
-      if (exists('modFit_PVQ1')) {
-        if (!class(modFit_PVQ1)[1] == 'mirt_df') {
-          rm(modFit_PVQ1)
+        
+        # check item fit indices are exists
+        
+        if (exists("modFit_Zh")) {
+            if (!class(modFit_Zh)[1] == "mirt_df") {
+                rm(modFit_Zh)
+            }
         }
-      }
-
-      if (exists('modFit_infit')) {
-        if (!class(modFit_infit)[1] == 'mirt_df') {
-          rm(modFit_infit)
+        
+        if (exists("modFit_SX2")) {
+            if (!class(modFit_SX2)[1] == "mirt_df") {
+                rm(modFit_SX2)
+            }
         }
-      }
-
-      itemFitList <-
-        c('modFit_Zh',
-          'modFit_SX2',
-          'modFit_PVQ1',
-          'modFit_infit')[c(
-            exists('modFit_Zh'),
-            exists('modFit_SX2'),
-            exists('modFit_PVQ1'),
-            exists('modFit_infit')
-          )]
-
-      fitList <- list()
-      for (i in 1:length(itemFitList)) {
-        # fitList[[i]] <-
-        fitList[[i]] <- (eval(parse(text = itemFitList[i])))
-      }
-      return(suppressMessages(plyr::join_all(fitList)))
-
+        
+        if (exists("modFit_PVQ1")) {
+            if (!class(modFit_PVQ1)[1] == "mirt_df") {
+                rm(modFit_PVQ1)
+            }
+        }
+        
+        if (exists("modFit_infit")) {
+            if (!class(modFit_infit)[1] == "mirt_df") {
+                rm(modFit_infit)
+            }
+        }
+        
+        itemFitList <- c("modFit_Zh", "modFit_SX2", "modFit_PVQ1", "modFit_infit")[c(exists("modFit_Zh"), 
+            exists("modFit_SX2"), exists("modFit_PVQ1"), exists("modFit_infit"))]
+        
+        fitList <- list()
+        for (i in 1:length(itemFitList)) {
+            # fitList[[i]] <-
+            fitList[[i]] <- (eval(parse(text = itemFitList[i])))
+        }
+        return(suppressMessages(plyr::join_all(fitList)))
+        
     } else {
-      message("That's seems not MIRT model, so that trying to estimate new model")
-      estModel <- exploratoryIRT(data = mirtModel)
-      return(estModel)
+        message("That's seems not MIRT model, so that trying to estimate new model")
+        estModel <- exploratoryIRT(data = mirtModel)
+        return(estModel)
     }
-  }
+}

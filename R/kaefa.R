82abddb5fb8f70e6aa6896ffa4516e489747562c
seonghobae <seongho@kw.ac.kr>
2017-09-20 23:11:53 +0900

    adjust CPU

--- R/kaefa.R
+++ R/kaefa.R
@@ -26,17 +26,17 @@ aefaInit <- function(GCEvms = NULL, debug = F) {
             parallelProcessors <- 2
         }
     } else if (suppressWarnings(NCmisc::top()$CPU$idle) > 50) {
-        parallelProcessors <- round(parallel::detectCores(all.tests = FALSE, logical = FALSE)/2)
+        parallelProcessors <- round(parallel::detectCores(all.tests = FALSE, logical = FALSE))
         if (2 >= parallelProcessors) {
             parallelProcessors <- 2
         }
     } else if (suppressWarnings(NCmisc::top()$CPU$idle) <= 50) {
-        parallelProcessors <- round(parallel::detectCores(all.tests = FALSE, logical = FALSE)/3)
+        parallelProcessors <- round(parallel::detectCores(all.tests = FALSE, logical = FALSE)/2)
         if (2 >= parallelProcessors) {
             parallelProcessors <- 2
         }
     } else if (suppressWarnings(NCmisc::top()$CPU$idle) < 30) {
-        parallelProcessors <- round(parallel::detectCores(all.tests = FALSE, logical = FALSE)/4)
+        parallelProcessors <- round(parallel::detectCores(all.tests = FALSE, logical = FALSE)/3)
         if (2 >= parallelProcessors) {
             parallelProcessors <- 2
         }
